b83203db588ed938bcdf1100a0963940f4882f95
Wanderson <wandersongamer2@gmail.com>
2022-09-19 21:11:29 -0300

    Questão 3

--- Lista-Faculdade/Lista1/ex3/Employee.java
+++ Lista-Faculdade/Lista1/ex3/Employee.java
@@ -5,7 +5,25 @@ public class Employee {
   private double grossSalary;
   private double tax;
 
+  public String getName() {
+    return this.name;
+  }
   public void setName(String name) {
     this.name = name;
+  }
+  public void setGrossSalary(double grossSalary) {
+    this.grossSalary = grossSalary;
+  }
+  public void setTax(double tax) {
+    this.tax = tax;
+  }
+
+  public double NetSalary() {
+    return this.grossSalary - this.tax;
+  }
+
+  public void increaseSalary(double percentage) {
+    double percent = (percentage/100) + 1;
+    this.grossSalary *= percent;
   }
 }
--- Lista-Faculdade/Lista1/ex3/Program.java
+++ Lista-Faculdade/Lista1/ex3/Program.java
@@ -1,26 +1,34 @@
+package ex3;
+
+import java.text.DecimalFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
   public static void main(String[] args) {
          Locale.setDefault(Locale.US);
          Scanner sc = new Scanner(System.in);
 
+         DecimalFormat df = new DecimalFormat("#,###.00");
+
          Employee emp = new Employee();
 
          System.out.print("Name: ");
-         emp.name = sc.nextLine();
+         emp.setName(sc.nextLine());
          System.out.print("Gross salary: ");
-         emp.grossSalary = sc.nextDouble();
+         emp.setGrossSalary(sc.nextDouble());
          System.out.print("Tax: ");
-         emp.tax = sc.nextDouble();
+         emp.setTax(sc.nextDouble());
 
          System.out.println();
-         System.out.println("Employee: " + emp);
+         System.out.println("Employee: " + emp.getName() + ", $ "+ df.format(emp.NetSalary()));
          System.out.println();
          System.out.print("Which percentage to increase salary? ");
          double percentage = sc.nextDouble();
          emp.increaseSalary(percentage);
 
          System.out.println();
-         System.out.println("Updated data: " + emp);
+         System.out.println("Updated data: " + emp.getName()+ ", $ "+ df.format(emp.NetSalary()));
          sc.close();
   }
 }
