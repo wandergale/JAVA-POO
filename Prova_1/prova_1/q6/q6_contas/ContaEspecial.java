b0f0f105a85186b581fa71bb74b348012a7d85f8
Wanderson <wandersongamer2@gmail.com>
2022-10-13 11:18:43 -0300

    sobrescrevendo metodo de transferencias para se adaptar com o limite da contaEspecial

--- Prova_1/prova_1/q6/q6_contas/ContaEspecial.java
+++ Prova_1/prova_1/q6/q6_contas/ContaEspecial.java
@@ -32,4 +32,18 @@ public class ContaEspecial extends ContaCorrente {
 	public void setLimite(float limite) {
 		this.limite = limite;
 	}
+	
+	@Override
+	public boolean transferir(ContaCorrente conta, float valor) {
+		if (valor <= this.saldo + this.limite) {			
+			this.saldo -= valor;
+			conta.depositar(valor);
+			System.out.println("Transferencia para "
+								+conta.getCliente().getNome() 
+								+ " realizada com sucesso");
+			return true;
+		}
+		System.out.println("Ocorreu um erro na operação");
+		return false;
+	}
 }
